0f05dfd5050c4ac221730068d45f77be641a5170
SameC137 <samemichael1415@gmail.com>
2021-07-18 08:38:00 +0300

    Setup deployment

---
+++ apps/correlation_app.py
@@ -0,0 +1,40 @@
+
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..')))
+sys.path.insert(0, '../scripts')
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from scripts.scripts import *
+from scripts.data_loaders import loadData1, loadData2
+
+def app():
+    st.title("Correlation ")
+    
+    st.markdown("Correlation between the different aggregated columns by users")
+
+    df=loadData1().copy()
+    df=form_combination_columns(df)
+    f={     'Bearer Id': 'count',
+        'Dur. (ms)':'sum',
+        'Social Media (Bytes)' : 'sum',
+        'Google (Bytes)' : 'sum', 
+        'Email (Bytes)' : 'sum', 
+        'Youtube (Bytes)' : 'sum', 
+        'Netflix (Bytes)' : 'sum', 
+        'Gaming (Bytes)' : 'sum', 
+        'Other (Bytes)' : 'sum', 
+        'Total Data (Bytes)' : 'sum', 
+    }
+
+
+    dfAgg=df.groupby(['MSISDN/Number']).agg(f)
+
+    fig4=plt.figure(figsize=(30, 16))    
+    sns.heatmap(dfAgg.corr(method='pearson'), cmap="YlGnBu", annot=True,linewidths=.5)
+    st.pyplot(fig4)
---
+++ apps/distribution_graphs.py
@@ -0,0 +1,34 @@
+
+
+    
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..')))
+sys.path.insert(0, '../scripts')
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from scripts.scripts import *
+from scripts.data_loaders import loadData1, loadData2
+
+def app():
+    st.title("Distribution Graphs ")
+    
+
+    df=loadData1().copy()
+    
+    df=form_combination_columns(df)
+    # print(df["Total Data (Bytes)"].head(5))
+    
+    column=st.selectbox('Column',["Social Media (Bytes)", "Youtube (Bytes)","Netflix (Bytes)", "Google (Bytes)", \
+         "Email (Bytes)", "Gaming (Bytes)", "Other (Bytes)" ,  "Total Data (Bytes)"])
+
+
+    fig4=plt.figure(figsize=(30, 16))    
+    sns.histplot(data=df[column], color='red', kde=True)
+    # plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+    st.pyplot(fig4)
