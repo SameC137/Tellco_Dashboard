b667fdb77b56eb31a14ad2fd6434e565c5476cfe
SameC137 <samemichael1415@gmail.com>
2021-07-18 10:02:04 +0300

    path

--- apps/data_loaders.py
+++ apps/data_loaders.py
@@ -7,11 +7,11 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 
 @st.cache(allow_output_mutation=True)
 def loadData1():
-    df=pd.read_csv('../data/cleaned_modified_data.csv')
+    df=pd.read_csv('data/cleaned_modified_data.csv')
     return df
 
 
 @st.cache
 def loadData2():
-    df_scored=pd.read_csv('../data/satisfaction_score.csv')
+    df_scored=pd.read_csv('data/satisfaction_score.csv')
     return df_scored
